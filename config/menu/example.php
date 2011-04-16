97936f442eaa86cef2bb2cc486a5a086fc5e8c4f
Dawid Boruta <diabelb@gmail.com>
2011-04-16 20:54:26 +0200

    added missing file

--- config/menu/example.php
+++ config/menu/example.php
@@ -2,7 +2,7 @@
 
 return array
 (
-
+        'driver' => 'database', // you can use 'database' or 'file', database uses ORM driver
 	'view' => 'menu', // the view file
 	'current_class' => 'current', // the set_current() method uses this setting to mark the current menu item
 
